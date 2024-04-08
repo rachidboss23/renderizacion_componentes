da3196ed675c5971339995bb3362823b87c9f38b
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-04-07 22:04:08 -0400

    cambio de ruta en archivo json propiedad home pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://EdgardJonh.github.io/Segundo_desafio_React-main'
+  base: 'https://EdgardJonh.github.io/renderizacion_componentes'
 })
